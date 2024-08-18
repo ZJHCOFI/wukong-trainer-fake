37322b2259426156807c948ce4615acd1309b4c6
ZJHCOFI <59951014+ZJHCOFI@users.noreply.github.com>
2024-08-18 17:23:28 +0800

    猿神游戏启动文件源代码
    猿神游戏启动文件源代码

---
+++ YuanShen_StartGame/Main_Form.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WuKong_YuanShen
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_Form));
+            this.pictureBox_Btn_Quit = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Btn_Minimized = new System.Windows.Forms.PictureBox();
+            this.pictureBox_GameBg = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Btn_Quit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Btn_Minimized)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_GameBg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_Btn_Quit
+            // 
+            this.pictureBox_Btn_Quit.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_Btn_Quit.Image = global::WuKong_YuanShen.Properties.Resources.quit_btn;
+            this.pictureBox_Btn_Quit.Location = new System.Drawing.Point(1220, 26);
+            this.pictureBox_Btn_Quit.Name = "pictureBox_Btn_Quit";
+            this.pictureBox_Btn_Quit.Size = new System.Drawing.Size(28, 28);
+            this.pictureBox_Btn_Quit.TabIndex = 3;
+            this.pictureBox_Btn_Quit.TabStop = false;
+            this.pictureBox_Btn_Quit.Click += new System.EventHandler(this.pictrueBox_Click);
+            this.pictureBox_Btn_Quit.MouseLeave += new System.EventHandler(this.pictrueBox_MouseLeave);
+            // 
+            // pictureBox_Btn_Minimized
+            // 
+            this.pictureBox_Btn_Minimized.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_Btn_Minimized.Image = global::WuKong_YuanShen.Properties.Resources.minimized_btn;
+            this.pictureBox_Btn_Minimized.Location = new System.Drawing.Point(1177, 26);
+            this.pictureBox_Btn_Minimized.Name = "pictureBox_Btn_Minimized";
+            this.pictureBox_Btn_Minimized.Size = new System.Drawing.Size(28, 28);
+            this.pictureBox_Btn_Minimized.TabIndex = 2;
+            this.pictureBox_Btn_Minimized.TabStop = false;
+            this.pictureBox_Btn_Minimized.Click += new System.EventHandler(this.pictrueBox_Click);
+            this.pictureBox_Btn_Minimized.MouseLeave += new System.EventHandler(this.pictrueBox_MouseLeave);
+            // 
+            // pictureBox_GameBg
+            // 
+            this.pictureBox_GameBg.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_GameBg.Image = global::WuKong_YuanShen.Properties.Resources.yuanshen;
+            this.pictureBox_GameBg.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_GameBg.Name = "pictureBox_GameBg";
+            this.pictureBox_GameBg.Size = new System.Drawing.Size(1280, 720);
+            this.pictureBox_GameBg.TabIndex = 0;
+            this.pictureBox_GameBg.TabStop = false;
+            this.pictureBox_GameBg.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_GameBg_MouseDown);
+            this.pictureBox_GameBg.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_GameBg_MouseMove);
+            this.pictureBox_GameBg.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_GameBg_MouseUp);
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.pictureBox_Btn_Quit);
+            this.Controls.Add(this.pictureBox_Btn_Minimized);
+            this.Controls.Add(this.pictureBox_GameBg);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Main_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "猿神，启动！";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_Form_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Btn_Quit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Btn_Minimized)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_GameBg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_GameBg;
+        private System.Windows.Forms.PictureBox pictureBox_Btn_Minimized;
+        private System.Windows.Forms.PictureBox pictureBox_Btn_Quit;
+    }
+}
+
---
+++ YuanShen_StartGame/Main_Form.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using WuKong_YuanShen.Properties;
+
+namespace WuKong_YuanShen
+{
+    public partial class Main_Form : Form
+    {
+        public Main_Form()
+        {
+            InitializeComponent();
+        }
+
+        //窗体移动相关的变量声明及赋值
+        private bool bool_Moving = false;
+        private Point oldMousePosition;
+
+        //窗口加载
+        private void Main_Form_Load(object sender, EventArgs e)
+        {
+            pictureBox_Btn_Minimized.MouseMove += new MouseEventHandler(pictrueBox_MouseMove);
+            pictureBox_Btn_Quit.MouseMove += new MouseEventHandler(pictrueBox_MouseMove);
+            pictureBox_GameBg.Controls.Add(pictureBox_Btn_Minimized);
+            pictureBox_GameBg.Controls.Add(pictureBox_Btn_Quit);
+            pictureBox_GameBg.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox_Btn_Minimized.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox_Btn_Quit.SizeMode = PictureBoxSizeMode.StretchImage;
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Resources.yuanshen_wav);
+            player.Play();
+        }
+
+        //事件：PictureBox点击事件
+        private void pictrueBox_Click(object sender, EventArgs e)
+        {
+            switch (((PictureBox)sender).Name.ToString())
+            {
+                case "pictureBox_Btn_Quit":
+                    Close();
+                    break;
+                case "pictureBox_Btn_Minimized":
+                    if (WindowState != FormWindowState.Minimized)
+                    {
+                        WindowState = FormWindowState.Minimized;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //事件：PictureBox鼠标经过事件
+        private void pictrueBox_MouseMove(object sender, EventArgs e)
+        {
+            switch (((PictureBox)sender).Name.ToString())
+            {
+                case "pictureBox_Btn_Quit":
+                    pictureBox_Btn_Quit.Cursor = Cursors.Hand;
+                    break;
+                case "pictureBox_Btn_Minimized":
+                    pictureBox_Btn_Minimized.Cursor = Cursors.Hand;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //事件：PictureBox鼠标离开事件
+        private void pictrueBox_MouseLeave(object sender, EventArgs e)
+        {
+            switch (((PictureBox)sender).Name.ToString())
+            {
+                case "pictureBox_Btn_Quit":
+                    pictureBox_Btn_Quit.Image = Resources.quit_btn;
+                    pictureBox_Btn_Quit.Cursor = Cursors.Default;
+                    break;
+                case "pictureBox_Btn_Minimized":
+                    pictureBox_Btn_Minimized.Image = Resources.minimized_btn;
+                    pictureBox_Btn_Minimized.Cursor = Cursors.Default;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //事件：鼠标左键按住窗口上方
+        private void pictureBox_GameBg_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (WindowState == FormWindowState.Maximized)
+            {
+                return;
+            }
+            pictureBox_GameBg.Cursor = Cursors.NoMove2D;
+            oldMousePosition = e.Location;
+            bool_Moving = true;
+        }
+
+        //事件：鼠标松开窗口上方
+        private void pictureBox_GameBg_MouseUp(object sender, MouseEventArgs e)
+        {
+            pictureBox_GameBg.Cursor = Cursors.Default;
+            bool_Moving = false;
+        }
+
+        //事件：鼠标左键拖动窗口上方
+        private void pictureBox_GameBg_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && bool_Moving)
+            {
+                Point newPosition = new Point(e.Location.X - oldMousePosition.X, e.Location.Y - oldMousePosition.Y);
+                Location += new Size(newPosition);
+            }
+        }
+
+        //事件：窗口关闭前的确认
+        private void Main_Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("您确认退出吗？\nAre you sure to quit?", "温馨提示 Tips", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Dispose();
+                Application.Exit();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
